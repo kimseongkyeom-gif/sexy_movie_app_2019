0e2dfddd74efc3fcf54c166536cdbeb6292e02c9
kimseongkyeom-gif <ep2535@naver.com>
2019-09-19 21:38:54 +0900

    상래야_빵먹고싶다

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Potato from "./Potato";
 
 function App() {
-  return <div><h1>Fucking shit</h1>
+  return <div><h1>Fucking shit 보이</h1>
     <Potato />
     </div>;
       
--- src/Potato.js
+++ src/Potato.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function Potato() {
-    return <h3>I Love Potato</h3>;
+    return <h3>I Love Potato
+        김상래 빵사와
+    </h3>;
 }
 export default Potato;
