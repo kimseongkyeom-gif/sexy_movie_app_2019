d6281e72c92c8d6eb7b68ed34b8fc2e4eb82b12a
kimseongkyeom-gif <ep2535@naver.com>
2019-09-20 01:38:22 +0900

    2-2강의

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-function Food({favorite}) {
+function Food({name,picture}) {
   
-  return <h1>I like {favorite} </h1>;
+  return<div> 
+    <h1>I like {name} </h1>
+    <img src={picture} />
+  </div>
 }
 
+const foodlike =[
+  {name: "Chicken",
+  image:
+  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FVqWQh6bFmtM%2Fmaxresdefault.jpg&f=1&nofb=1"
+  },
+  {name: "Jokbal",
+  image:
+  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FrRQIRcQqb4k%2Fmaxresdefault.jpg&f=1&nofb=1"
+  },
+  {
+  name: "Bossam",
+  image:
+  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwordfromtherok.files.wordpress.com%2F2012%2F10%2Fbossam.jpeg&f=1&nofb=1"
+  },
+  {name: "Makguksu",
+  image:
+  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fhapskorea.com%2Fwp-content%2Fuploads%2F2017%2F06%2Fmakguksu-.jpg&f=1&nofb=1"
+  }
+
+];
 function App() {
-  return <div><h1>Fucking shit 보이</h1>
-    <Food favorite="kimchi"/>  
-    <Food favorite="삼겹살"/>  
-    <Food favorite="마늘"/>  
-    <Food favorite="소고기"/>  
+  return <div>
+    {foodlike.map(dish => <Food name={dish.name} picture={dish.image}/>) }
     </div>;
       
   
