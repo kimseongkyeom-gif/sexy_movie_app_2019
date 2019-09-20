648a8fc0543a92127e9b28526440322a0ce99f8f
kimseongkyeom-gif <ep2535@naver.com>
2019-09-20 21:06:23 +0900

    3-0강

--- src/App.js
+++ src/App.js
@@ -1,69 +1,27 @@
 import React from 'react';
 import PropTypes from "prop-types"
 
-const foodlike =
-[
-  {id:1,
-    name: "Chicken",
-  image:
-  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FVqWQh6bFmtM%2Fmaxresdefault.jpg&f=1&nofb=1",
-   rating: 5 
-},
-{
-    id:2,
-    name: "Jokbal",
-  image:
-  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FrRQIRcQqb4k%2Fmaxresdefault.jpg&f=1&nofb=1",
-  rating: 4.9 
-},
-{
-  id:3,
-  name: "Bossam",
-  image:
-  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwordfromtherok.files.wordpress.com%2F2012%2F10%2Fbossam.jpeg&f=1&nofb=1",
-  rating: 4.8
-},
-{
-    id:4,
-    name: "Makguksu",
-  image:
-  "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fhapskorea.com%2Fwp-content%2Fuploads%2F2017%2F06%2Fmakguksu-.jpg&f=1&nofb=1",
-    rating: 4.7
-}
-
-];
-
-function Food({name,picture,rating}) {
-   
-  return (
-  <div> 
-    <h1>I like {name} </h1>
-    <h4>{rating}/5.0 </h4>
-    <img src={picture} alt={name} />
-  </div>
-  );
-}
-
-Food.propTypes={
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number
-};
-
-function App() {
-  return (
-   <div>
-    {foodlike.map(dish=> (
-      <Food key={dish.id} 
-      name={dish.name}
-       picture={dish.image}
-       rating ={dish.rating}
-       />
-    ))}
-    </div>
-      );
+class App extends React.Component{
+  state={
   
-}
+    count:0
 
+  };
+  add=()=>{
+    console.log("add");
+  };
+  minus=()=>{ 
+    console.log("minus");
+  };
+  render(){
+    return (<div>
+      <h1>The number is:{this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+  </div>
+   );
+}
+}
+ 
 export default App;
 
