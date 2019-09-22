260daeac5f43a569092baef47d4e9d84e51f429d
kimseongkyeom-gif <ep2535@naver.com>
2019-09-22 21:10:46 +0900

    3-2강

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from "prop-types"
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
+  
   state={
   
     count:0
@@ -13,8 +18,18 @@ class App extends React.Component{
   minus=()=>{ 
     this.setState(current =>({ count: current.count -1 }));
   };
+  componentDidMount(){
+    console.log("component renderd");
+  }
+  componentDidUpdate(){
+    console.log("I'm just updated");
+  }
+  componentWillUnmount(){
+    console.log("Good Bye. cruel world!")
+  }
   render()
   {
+    console.log("I'm rendering");
     return (
     <div>
       <h1>The number is:{this.state.count}</h1>
