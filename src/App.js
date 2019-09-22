9798dff284518994c77297d9a6661fa3e03918cf
kimseongkyeom-gif <ep2535@naver.com>
2019-09-22 20:30:53 +0900

    3-1강

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ class App extends React.Component{
 
   };
   add=()=>{
-    console.log("add");
+    this.setState(current =>({ count: current.count +1 }));
   };
   minus=()=>{ 
-    console.log("minus");
+    this.setState(current =>({ count: current.count -1 }));
   };
-  render(){
-    return (<div>
+  render()
+  {
+    return (
+    <div>
       <h1>The number is:{this.state.count}</h1>
       <button onClick={this.add}>Add</button>
       <button onClick={this.minus}>Minus</button>
